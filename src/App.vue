ca97a605faff0e8afa80fd4fc61d2e36f7cbc3fc
saishashank04 <shashank@cloudmpower.com>
2021-04-30 12:28:30 +0530

    second commit

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,10 @@ import HelloWorld from "./components/HelloWorld.vue";
 import NewComp from "./components/NewComp.vue";
 // This starter template is using Vue 3 experimental <script setup> SFCs
 // Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
+
+fetch("https://vite-test.shashank8178.workers.dev/").then((response) =>
+  console.log(response)
+);
 </script>
 
 <style>
---
+++ src/router.js
@@ -0,0 +1,22 @@
+// import { createWebHistory, createRouter } from "vue-router";
+// import Login from "@/components/Login.vue";
+// import App from "@/components/App.vue";
+// const routes = [
+// 	{
+// 		path: "/",
+// 		name: "App",
+// 		component: App,
+// 	},
+// 	{
+// 		path: "/login",
+// 		name: "Login",
+// 		component: Login,
+// 	},
+// ];
+
+// const router = createRouter({
+// 	history: createWebHistory(),
+// 	routes,
+// });
+
+// export default router;
